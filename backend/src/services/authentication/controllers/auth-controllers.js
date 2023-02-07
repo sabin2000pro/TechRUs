c4a21bc71ef8e0ec5e11520a638674e0381a66a7
sabin2000pro <sabinlungu293@gmail.com>
2023-02-07 13:32:00 +0000

    IMplementing API for authentication

--- backend/src/services/authentication/controllers/auth-controllers.js
+++ backend/src/services/authentication/controllers/auth-controllers.js
@@ -3,16 +3,74 @@ const asyncHandler = require('express-async-handler');
 
 module.exports.registerUser = asyncHandler(async (request, response, next) => {
 
+    try {
+
+    } 
+    
+    catch(error) {
+
+    }
+
 })
 
 module.exports.verifyEmailAddress = asyncHandler(async (request, response, next) => {
+ try {
 
+    } 
+    
+    catch(error) {
+
+    }
 })
 
 module.exports.loginUser = asyncHandler(async (request, response, next) => {
+    try {
+
+    } 
     
+    catch(error) {
+
+    }
 })
 
 module.exports.verifyLoginMFA = asyncHandler(async (request, response, next) => {
+    try {
+
+    } 
+    
+    catch(error) {
+
+    }
+})
+
+module.exports.forgotPassword = asyncHandler(async (request, response, next) => {
+    try {
+
+    } 
+    
+    catch(error) {
+
+    }
+})
+
+module.exports.resetPassword = asyncHandler(async (request, response, next) => {
+    try {
+
+    } 
+    
+    catch(error) {
+
+    }
+})
+
+module.exports.fetchLoggedInUser = asyncHandler(async (request, response, next) => {
+    try {
+
+    } 
+    
+    catch(error) {
+
+    }
+
     
 })
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Header from './components/Header'
+import {Routes, Route} from 'react-router-dom';
 
 const App = () => {
 
