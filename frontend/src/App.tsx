b8fa2e6ea0223519aa90fa73ece7e2acd48831b4
sabin2000pro <sabinlungu293@gmail.com>
2023-02-07 15:14:43 +0000

    Adding routes on the frontend for register, login, verify-email, all products, create product etc..

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,14 @@
 import { useState } from 'react'
+import Header from './components/Header'
+import {Routes, Route} from 'react-router-dom';
 
 const App: React.FC = () => {
 
   return (
 
      <>
+        <Header />
+
         
      </>
 
