97db0e069030c6cbcdf3ffb5d30e80f19bc97a90
sabin2000pro <sabinlungu293@gmail.com>
2023-02-07 15:16:06 +0000

    Adding routes on the frontend for register, login, verify-email, all products, create product etc..

--- backend/src/services/authentication/controllers/auth-controllers.js
+++ backend/src/services/authentication/controllers/auth-controllers.js
@@ -1,6 +1,7 @@
 const User = require('../models/user-model');
 const asyncHandler = require('express-async-handler');
 const {StatusCodes} = require('http-status-codes');
+const { isValidObjectId } = require('mongoose');
 
 module.exports.registerUser = asyncHandler(async (request, response, next) => {
 
@@ -13,6 +14,7 @@ module.exports.registerUser = asyncHandler(async (request, response, next) => {
         if(error) {
             return response.status()
         }
+        
     }
 
 
@@ -20,13 +22,26 @@ module.exports.registerUser = asyncHandler(async (request, response, next) => {
 })
 
 module.exports.verifyEmailAddress = asyncHandler(async (request, response, next) => {
- try {
+
+    try {
+        const {userId, OTP} = request.body;
+
+        if(!isValidObjectId(userId)) {
+
+        }
+
+        if(OTP === null) { // If there is no OTP present
+            // Send back error response
+        } 
+
 
     } 
     
     catch(error) {
 
     }
+
+
 })
 
 module.exports.loginUser = asyncHandler(async (request, response, next) => {
