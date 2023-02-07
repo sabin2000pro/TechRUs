2741865b69dabc85aa38d2845f1359b8cf5973b7
sabin2000pro <sabinlungu293@gmail.com>
2023-02-07 14:11:06 +0000

    preparing template api on the frontend for registering, logging in users

---
+++ frontend/src/api/auth-api.ts
@@ -0,0 +1,12 @@
+
+export const registerUser = async(registerPayload: any) => {
+
+}
+
+export const verifyUserEmailAddress = async (payload: any) => {
+
+}
+
+export const loginUser = async (loginPayload: any) => {
+    
+}
