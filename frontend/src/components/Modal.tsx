423150e507715e183a4e22704985a45d12263188
sabin2000pro <sabinlungu293@gmail.com>
2023-02-09 10:42:59 +0000

    Added MongoDB database image in the docker compose file

--- frontend/src/components/Modal.tsx
+++ frontend/src/components/Modal.tsx
@@ -1,16 +1,23 @@
 import React from 'react'
 
+// Pre Conditions: 1. The modal is closed before clicking the button to open it AND the modal is NOT already open
+// Post Conditions: 1. Modal is open when the toggle modal function is invoked AND the is modal open flag is set to true
+
 export interface IModalProps { // Props for the modal (opening and closing the modal)
     closeModal: () => void
-    openModal: () => void
+    toggleModal: () => void
     isModalOpen: boolean | null
 }
 
-export const Modal: React.FC<IModalProps> = ({closeModal, openModal, isModalOpen}) => {
+export const Modal: React.FC<IModalProps> = ({closeModal, toggleModal, isModalOpen}) => { // Modal component
 
   return (
-    
+
     <>
+
+     <div>
+        <button onClick = {toggleModal}>View Details</button>
+     </div>
 
     </>
 
