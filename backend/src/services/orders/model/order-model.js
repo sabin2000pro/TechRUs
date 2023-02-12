19249782ac35b7d6d152b01b2d2f2371e82f9138
sabin2000pro <sabinlungu293@gmail.com>
2023-02-12 21:55:33 +0000

    Creating the data model for the orders service

--- backend/src/services/orders/model/order-model.js
+++ backend/src/services/orders/model/order-model.js
@@ -2,15 +2,41 @@ const mongoose = require('mongoose');
 
 const OrderSchema = new mongoose.Schema({
 
-    "customerId": {
+    orderId: {
+        type: String,
+        required: [true, "The Order must have a valid order ID"]
+    },
+
+    customerId: {
         type: mongoose.Schema.Types.ObjectId,
         ref: "Customer",
         required: [true, "Please specify the Customer ID for this order"]
     },
 
-    "paymentMethod": {
+    paymentMethod: {
         type: String,
         required: [true, "Please specify the method used for this order"]
+    },
+
+    shippingFee: {
+        type: Number,
+        required: [true, "Please specify what the shipping fee is for this order"]
+    },
+
+    currency: {
+        type: String,
+        required: [true, "Please specify the currency used for this payment method"],
+        default: "GBP"
+    },
+
+    totalPrice: { // Total price for this order
+        type: Number,
+        required: [true, "Please specify the total price for this order"]
+    },
+
+    orderDate: {
+        type: Date,
+        required: true
     }
 
 }, {timestamps: true});
