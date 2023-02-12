492fd6f605d6ce4de334bfe81d8fc8dad2b6f42e
sabin2000pro <sabinlungu293@gmail.com>
2023-02-12 22:03:10 +0000

    Creating the data model for the orders service

--- backend/src/services/orders/model/order-model.js
+++ backend/src/services/orders/model/order-model.js
@@ -13,17 +13,23 @@ const OrderSchema = new mongoose.Schema({
         required: [true, "Please specify the Customer ID for this order"]
     },
 
-    paymentMethod: {
-        type: String,
-        required: [true, "Please specify the method used for this order"]
-    },
+    items: [ // Payment items stores the products that have been ordered. Including the Product Id, name
 
-    shippingFee: {
+        {
+            productId: "",
+            name: "",
+            price: 0,
+            countInStock: 0
+        }
+
+    ],
+
+    shippingFee: { // Shipping Fee incurred by this order
         type: Number,
         required: [true, "Please specify what the shipping fee is for this order"]
     },
 
-    currency: {
+    currency: { // Currency used to pay for this order
         type: String,
         required: [true, "Please specify the currency used for this payment method"],
         default: "GBP"
@@ -43,7 +49,23 @@ const OrderSchema = new mongoose.Schema({
         type: String,
         required: [true, "Please specify the status that the order is in"],
         enum: ['created', 'pending', 'shipped', 'processing', 'canceled', 'refunded']
-    }
+    },
+
+    payment: { // Payment object will hold the payment used for the payment, transaction ID, the status of the payment such as Pending, Failed or Canceled
+        paymentMethod: String,
+        transactionId: String,
+
+        paymentStatus: {
+            type: String,
+            default: "pending",
+            enum: ['pending', 'failed', 'canceled']
+        },
+
+        paymentAmount: Number,
+        paymentCurrency: String
+    },
+
+    customerNotes: String
 
 }, {timestamps: true});
 
