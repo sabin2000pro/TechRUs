e9dee3e2d4fd92c06b11080fa8ea9a583bfde119
sabin2000pro <sabinlungu293@gmail.com>
2023-02-13 15:48:25 +0000

    Configuring the coupons service

--- backend/services/coupons/coupons-server.js
+++ backend/services/coupons/coupons-server.js
@@ -7,4 +7,24 @@ const app = express();
 const couponPort = process.env.COUPONS_PORT || 5402
 
 // Mount the middleware
+app.use(express.json());
+app.use(morgan('dev'));
 
+app.use(cors({
+    origin: "*",
+    methods: ["POST", "PUT", "DELETE", "PATCH", "GET"]
+}))
+
+// Mount the Coupon API Endpoints
+
+const couponsServer = app.listen(couponPort, (error) => {
+    try {
+
+    } 
+    
+    catch(error) {
+
+    }
+})
+
+module.exports = couponsServer;
