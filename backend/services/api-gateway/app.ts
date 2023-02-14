3345c4f82ee49dcca41846a9f23528145715292b
sabin2000pro <sabinlungu293@gmail.com>
2023-02-14 15:26:47 +0000

    Configuring API gateway and event bus services

--- backend/services/api-gateway/app.ts
+++ backend/services/api-gateway/app.ts
@@ -2,9 +2,10 @@ import express from "express";
 import cors from 'cors';
 import morgan from 'morgan';
 import axios from 'axios';
-
 const app = express();
 
 const API_GATEWAY_PORT = process.env.API_GATEWAY_PORT || 6000;
-const DEV_MODE = process.env.API_GATEWAY_DEV_MODE || 'development'
+const API_GATEWAY_DEV_MODE = process.env.API_GATEWAY_DEV_MODE || 'development'
 
+app.use(express.json());
+app.use(morgan('dev'));
