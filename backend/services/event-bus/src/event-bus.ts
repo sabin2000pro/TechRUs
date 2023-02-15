c962a73340a8da2f6cc43a250eb48ef70cd68cff
sabin2000pro <sabinlungu293@gmail.com>
2023-02-15 09:53:30 +0000

    Configuring Dockerfile to create an image out of the event bus container

--- backend/services/event-bus/src/event-bus.ts
+++ backend/services/event-bus/src/event-bus.ts
@@ -1,27 +1,38 @@
+interface Events {
+    [key: string]: Function[];
+  }
+
 class EventBus {
-    public events: {};
+    events: Events; // Map events to the events interface
+    public eventReceived: boolean;
+    public eventEmitted: boolean
 
     constructor() {
-        this.events = {}
+        this.events = {};
+        this.eventReceived = false;
+        this.eventEmitted = false;
     }
 
-    public onEventReceived(event, callback) {
+    public onEventReceived(event: any, callback: Function) {
 
         if(!this.events[event]) {
             this.events[event] = []
         }
 
         else {
+            this.eventReceived = true;
             this.events[event].push(callback);
         }
 
 
     }
 
-    public emitEvent(event, currentData) { // Function to emit an event once an action has occurred
+    public emitEvent(event: any, currentData: any) { // Function to emit an event once an action has occurred
         const currentEvents = this.events[event];
 
         if (currentEvents) {
+            this.eventEmitted = true;
+
             currentEvents.forEach(callback => {
             callback(currentData);
       });
---
+++ backend/services/event-bus/src/index.ts
@@ -0,0 +1,4 @@
+import dotenv from 'dotenv';
+import express, {Request, Response, NextFunction} from 'express';
+import morgan from 'morgan';
+import axios from 'axios';
