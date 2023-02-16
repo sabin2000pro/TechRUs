a3d2ad313d2495cdd5611e67194bb9db88541c36
sabin2000pro <sabinlungu293@gmail.com>
2023-02-16 14:04:02 +0000

    Setting up root route for payments service and launching the docker image

--- backend/services/payments/src/app.ts
+++ backend/services/payments/src/app.ts
@@ -1,6 +1,7 @@
-import express, {Application} from 'express';
+import express, {Application, Response, NextFunction} from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
+import {StatusCodes} from 'http-status-codes'
 
 const app: Application = express();
 
@@ -11,6 +12,22 @@ app.use(cors({
     origin: "*",
     methods: ['GET', "POST", 'PUT', "DELETE", 'PATCH']
 }))
+
+app.get('/', async (request: any, response: Response, next: NextFunction): Promise<any> => {
+    
+    try {
+        return response.status(StatusCodes.OK).json({success: true, message: "Payments Service Root Route"});
+    } 
+    
+    catch(error) {
+
+      if(error) {
+        return response.status(StatusCodes.BAD_REQUEST).json({success: false, message: error.message});
+      }
+
+    }
+
+})
 
 // app.use('/api/auth', authRouter);
 
