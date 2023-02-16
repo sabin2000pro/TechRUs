58899b86191fa106daf4940e73d72d833e3ea758
sabin2000pro <sabinlungu293@gmail.com>
2023-02-16 14:55:40 +0000

    Configuring package.json file in orders service

--- backend/services/orders/src/app.ts
+++ backend/services/orders/src/app.ts
@@ -1,1 +1,21 @@
-import express, {Request, Response, NextFunction} from 'express';
+import dotenv from 'dotenv';
+dotenv.config({path: './config.env'})
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+
+const app = express();
+
+app.use(express.json());
+
+if(process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+
+app.use(cors({
+    origin: '*',
+    methods: ["POST", "GET", 'POST']
+}))
+
+
+export {app}
