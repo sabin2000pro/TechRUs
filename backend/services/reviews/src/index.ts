c061a6f2972ef43e9de008700f810c889a0a388f
sabin2000pro <sabinlungu293@gmail.com>
2023-02-16 14:48:03 +0000

    Finished configuring the reviews service

---
+++ backend/services/reviews/src/index.ts
@@ -0,0 +1,25 @@
+import {app} from './app';
+
+const REVIEWS_SERVICE_PORT = process.env.REVIEWS_SERVICE_PORT || 5407;
+
+export const listenReviewsServer = () => { // Create the product server to handle GET, POST, DELETE, PUT request 
+    
+    try {    
+
+        return app.listen(REVIEWS_SERVICE_PORT, () => {
+            console.log(`The Reviews Service server listening for requests on port ${REVIEWS_SERVICE_PORT} in mode: ${process.env.PRODUCT_SERVICE_NODE_ENV}`);
+        })
+
+    } 
+    
+    catch(error) {
+
+        if(error) {
+            return console.error(error);
+        }
+
+    }
+
+}
+
+listenReviewsServer();
