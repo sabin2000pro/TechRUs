9dc488afecc3928b094f19210f78ef2b42c88b66
sabin2000pro <sabinlungu293@gmail.com>
2023-02-16 14:11:49 +0000

    Establishing a connection to the payments service database

--- backend/services/payments/src/database/payments-schema.ts
+++ backend/services/payments/src/database/payments-schema.ts
@@ -3,12 +3,29 @@ import mongoose from 'mongoose';
 
 const PAYMENTS_DB_URI = process.env.PAYMENTS_DB_URI || "";
 
-export const connectPaymentsSchema = (): Promise<any> => {
+export const connectPaymentsSchema = async (): Promise<any> => {
     try {
-       return await
+
+       return await mongoose.connect(PAYMENTS_DB_URI).then(conn => {
+
+        if(conn.connection) {
+            return console.log(`Connected to the payments schema succesfully...`);
+        }
+
+        else {
+            return console.log(`Could not connect to the payments schema`)
+        }
+
+
+       })
     } 
     
     catch(error) {
+        
+       if(error) {
+        return console.error(error);
+       }
+
 
     }
 }
