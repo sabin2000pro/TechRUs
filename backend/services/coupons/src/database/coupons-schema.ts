d99fbd1b53630456db871f2d5d3a9005ff1207e7
sabin2000pro <sabinlungu293@gmail.com>
2023-03-01 16:17:56 +0000

    Establishing connection to the coupons schema dB

--- backend/services/coupons/src/database/coupons-schema.ts
+++ backend/services/coupons/src/database/coupons-schema.ts
@@ -7,10 +7,28 @@ const COUPONS_SERVICE_DB_URI = process.env.COUPONS_SERVICE_DB_URI;
 export const connectCouponsSchema = async () => {
 
     try {
-        return await mongoose.connect(COUPONS_SERVICE_DB_URI)
+        return await mongoose.connect(COUPONS_SERVICE_DB_URI!).then(conn => {
+
+            if(conn.connection) {
+                return console.log(`Connected to the coupons schema successfully`);
+            }
+
+            else {
+                return console.log(`Could not connect to the coupons schema`)
+            }
+
+
+        })
     } 
     
     catch(error) {
+        
+        if(error) {
+            return console.error(error);
+        }
 
-    }
+
+    }    
+
+
 }
