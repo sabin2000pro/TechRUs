d2a2970cfdeb5b1ba8a43ffc5e43e6489af75135
sabin2000pro <sabinlungu293@gmail.com>
2023-03-01 16:18:45 +0000

    Establishing connection to the coupons schema dB

--- backend/services/coupons/src/index.ts
+++ backend/services/coupons/src/index.ts
@@ -1,14 +1,13 @@
+require('dotenv').config();
 import { app } from "./app"
-import dotenv from 'dotenv';
-dotenv.config({path: "/Users/sabin2000/Documents/TechRUs/backend/services/coupons/config.env"});
 
-const port = process.env.PORT || 5402;
+const COUPONS_SERVICE_PORT = process.env.COUPONS_SERVICE_PORT || 5402;
 
 // Start of authentication server
 export const startCouponsServer = async () => {
 
-      return app.listen(port, () => {
-        console.log(`Coupons Service Server is listening for requests on port ${port} in mode: ${process.env.NODE_ENV}`);
+      return app.listen(COUPONS_SERVICE_PORT, () => {
+           console.log(`Coupons Service Server is listening for requests on port ${COUPONS_SERVICE_PORT} in mode: ${process.env.NODE_ENV}`);
       });
 
 }
