3d22bd109a2a2c528973835951cd8ed48c6b1b71
sabin2000pro <sabinlungu293@gmail.com>
2023-03-05 20:41:25 +0000

    Removed the categories service for just now only dealing with tech products

--- backend/services/categories/src/routes/category-routes.ts
+++
@@ -1,3 +0,0 @@
-import express from 'express';
-
-export const categoriesRouter = express.Router({mergeParams: true});
---
+++ frontend/src/components/shipping/ShippingScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ShippingScreen = () => {
+  return (
+
+    <div>
+
+    </div>
+  )
+}
+
+export default ShippingScreen
