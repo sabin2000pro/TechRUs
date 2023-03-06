9cdb20ce4dfa35c4b9d69b2593a690c509ccfd60
sabin2000pro <sabinlungu293@gmail.com>
2023-03-06 14:47:42 +0000

    Adding start / end shift data attributes to the users data model

--- backend/services/api-gateway/src/app.ts
+++ backend/services/api-gateway/src/app.ts
@@ -6,16 +6,20 @@ import {apiGatewayRouter} from './routes/api-gateway-routes';
 
 const app: Application = express();
 
+if(process.env.API_GATEWAY_DEV_MODE === 'development') {
+    app.use(morgan('dev'));
+}
+
 app.use(express.json());
-app.use(morgan('dev'));
+
 app.use(cors({
-    origin: "*"
+    origin: "*",
 }))
 
 app.get('/', async (request: any, response: Response, next: NextFunction): Promise<any> => {
     return response.status(StatusCodes.OK).json({success: true, message: "API Gateway Entrypoint"});
 })
 
-app.use('/api/api-gateway', apiGatewayRouter);
+app.use('/api/v1/api-gateway', apiGatewayRouter);
 
 export {app};
