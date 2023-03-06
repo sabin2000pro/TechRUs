e2c28f984bd37ae74ab2f575eb71aa03426c0b4e
sabin2000pro <sabinlungu293@gmail.com>
2023-03-06 10:10:14 +0000

    Creating the controller functions for the payments service. One of the controllers returns the stripe client key to be consumed by the frontend

--- backend/services/payments/src/controllers/payment-controllers.ts
+++ backend/services/payments/src/controllers/payment-controllers.ts
@@ -1,14 +1,21 @@
+import { StatusCodes } from 'http-status-codes';
 import asyncHandler from 'express-async-handler';
 require('dotenv').config();
 import stripe from 'stripe'
 import { IPaymentsParams } from '../interface/payments-interface';
+import {Response, NextFunction} from 'express';
 
 const STRIPE_API_KEY = process.env.STRIPE_API_KEY || ""
+const STRIPE_PUBLISHABLE_KEY = process.env.STRIPE_PUBLISHABLE_KEY || ""
 
 export const createNewPayment = asyncHandler(async (request, response, next) => {
-    
+    const {} = request.body;
 })
 
-export const fetchClientSecretKey = asyncHandler(async (request, response, next) => {
+export const fetchClientSecretKey = asyncHandler(async (request: any, response: Response, next: NextFunction): Promise<Response | any> => {
+    return response.status(StatusCodes.OK).json({success: true, client_secret: STRIPE_API_KEY});
+})
+
+export const fetchClientPublishableKey = asyncHandler(async (request: any, response: Response, next: NextFunction) => {
 
 })
