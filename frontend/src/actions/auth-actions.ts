121db76360f2b113c652d8b08ba6cad514de46a9
sabin2000pro <sabinlungu293@gmail.com>
2023-03-06 09:45:44 +0000

    Creating the boilerplate code for the authentication actions / reducers

--- frontend/src/actions/auth-actions.ts
+++ frontend/src/actions/auth-actions.ts
@@ -1,1 +1,38 @@
 import axios from 'axios';
+
+
+export const register = (username: string, email: string, password: string) => async (dispatch) => {
+    try {
+      
+    } 
+    
+    catch(error) {
+
+    }
+} 
+
+export const login = (email: string, password: string) => async (dispatch) => {
+    try {
+
+    } 
+    
+    catch(error) {
+
+    }
+
+
+} 
+
+
+export const fetchLoggedInCustomer = () => async (dispatch) => {
+
+    try {
+
+    } 
+    
+    catch(error) {
+
+    }
+
+    
+} 
