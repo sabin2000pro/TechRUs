83cd35184b41ca98e10cff1120c489069eab4042
sabin2000pro <sabinlungu293@gmail.com>
2023-03-06 10:00:29 +0000

    Working on the payment model by adding an interface to the model

--- backend/services/payments/src/models/payment-model.ts
+++ backend/services/payments/src/models/payment-model.ts
@@ -58,7 +58,8 @@ export const PaymentSchema = new mongoose.Schema<IPaymentDocument>({
       },
 
       updatedAt: {
-
+         type: Date,
+         default: Date.now
       }
 
 }, {timestamps: true});
