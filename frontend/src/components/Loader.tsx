8d09437d37619fa629ca5626ede80847b9778010
sabin2000pro <sabinlungu293@gmail.com>
2023-03-06 13:30:54 +0000

    Changed the users.json file to reflect staff / admin / store manager roles for the system

---
+++ frontend/src/components/Loader.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Loader = () => {
+    
+  return (
+
+    <>
+
+        <div className = "loader-wrapper">
+
+        </div>
+
+    </>
+
+  )
+}
+
+export default Loader
