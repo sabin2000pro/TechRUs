6cd053570125eea30bd1ac3ed17600c278db31ac
sabin2000pro <sabinlungu293@gmail.com>
2023-03-07 16:04:55 +0000

    Working on the users service to test the API for fetching, updating and deleting users

--- backend/services/users-service/src/index.ts
+++ backend/services/users-service/src/index.ts
@@ -1,13 +1,13 @@
 require('dotenv').config();
 import { app } from "./app"
 
-const CUSTOMERS_SERVICE_PORT = process.env.CUSTOMERS_SERVICE_PORT || 5420;
+const USERS_SERVICE_PORT = process.env.USERS_SERVICE_PORT || 5420;
 
 // Start of authentication server
 export const startUserServer = async () => {
 
-      return app.listen(CUSTOMERS_SERVICE_PORT, () => {
-        console.log(`Coupons Service Server is listening for requests on port ${CUSTOMERS_SERVICE_PORT} in mode: ${process.env.CUSTOMERS_DEV_MODE}`);
+      return app.listen(USERS_SERVICE_PORT, () => {
+        console.log(`Users Service is listening for requests on port ${USERS_SERVICE_PORT} in mode: ${process.env.USERS_DEV_MODE}`);
       });
 
 }
