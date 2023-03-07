b9c965f0d2e6396e48a71298517e637cb2d1f1e0
sabin2000pro <sabinlungu293@gmail.com>
2023-03-07 10:47:21 +0000

    Working on the products service API

--- backend/services/products/src/controllers/products-controllers.ts
+++ backend/services/products/src/controllers/products-controllers.ts
@@ -1,20 +1,18 @@
+import { ErrorResponse } from './../../../authentication/src/utils/error-response';
 import {Product} from '../model/products-model';
 import {Response, NextFunction} from 'express'
 import asyncHandler from 'express-async-handler';
 import {StatusCodes} from 'http-status-codes';
 import {isValidObjectId} from 'mongoose';
-import { ProductsAPIFeatures } from '../utils/api-features';
 
 export const fetchAllProducts = asyncHandler(async (request: any, response: Response, next: NextFunction): Promise<any> => {
+        const pagination = {} // Create object for pagination
+        const queryCopy = {...request.query};
 
-    try {
-        const query = request.query
+        const page = parseInt(request.query.page) || 1;
         const productsPerPage = 4;
-
-        const apiFeatures = new ProductsAPIFeatures(Product.find(), query).search().paginate(productsPerPage)
-        const products = await apiFeatures!.query
-
-        console.log(`Products : `, products)
+        
+        const products = await Product.find();
 
         if(!products) {
             return response.status(StatusCodes.BAD_REQUEST).json({success: false, message: "No products found"})
@@ -22,59 +20,37 @@ export const fetchAllProducts = asyncHandler(async (request: any, response: Resp
 
         return response.status(StatusCodes.OK).json({success: true, products})
     }
-    
-    catch(error)  {
-
-        if(error) {
-            return response.status(StatusCodes.INTERNAL_SERVER_ERROR).json({success: false, message: error.message})
-        }
-
-    }
-
-})
+)
 
 export const fetchSingleProductByID = asyncHandler(async (request: any, response: Response, next: NextFunction): Promise<any> => {
 
-    try {
+        const id = request.params.id;
+        const product = await Product.findById(id);
 
-        const productId = request.params.productId;
-        const product = await Product.findById(productId);
-
-        if(product === null) {
-            return response.status(StatusCodes.BAD_REQUEST).json({success: false, message: `No product with ID : ${productId} found`});
+        if(!isValidObjectId(id)) {
+           return next(new ErrorResponse(`Product with that ID cannot be found`, StatusCodes.BAD_REQUEST));
         }
 
         return response.status(StatusCodes.OK).json({success: true, product});
     }
     
-    catch(error) {
-
-        if(error) {
-            return response.status(StatusCodes.INTERNAL_SERVER_ERROR).json({success: false, message: error.message})
-        }
-
-
-    }
-
-
-})
+)
 
 export const createNewProduct = asyncHandler(async (request: any, response: Response, next: NextFunction): Promise<any> => {
 
-    try {
-       request.body.customer = request.customer._id;
-       const productBody = request.body;
-       const product = await Product.create(productBody);
+       request.body.user = request.user._id; // When creating a product, add the currently logged in user to the body of the request
+       const {name, description, warranty, image, price, stockCount, lowStockAlert, isNew} = request.body;
 
+       if(!name.toString() || !description.toString() || !warranty.toString() || !image.toString() || !price.toString() || !stockCount || !lowStockAlert || !isNew) {
+           return next(new ErrorResponse(`Some entries are missing. Please check again when creating a product`, StatusCodes.BAD_REQUEST));
+       }
+
+       const product = await Product.create({name, description, warranty, image, price, stockCount, lowStockAlert, isNew});
        await product.save();
-    }
-    
-    catch(error) {
-
-        return response.status(StatusCodes.INTERNAL_SERVER_ERROR).json({success: false, message: error.message})
+       return response.status(StatusCodes.CREATED).json({success: true, product});
     }
 
-})
+)
 
 export const fetchNewProducts = asyncHandler(async (request: any, response: Response, next: NextFunction) => {
     const newProducts = await Product.find({isNew: true});
--- frontend/src/reducers/products-reducers.ts
+++ frontend/src/reducers/products-reducers.ts
@@ -1,4 +1,5 @@
-import { FETCH_ALL_PRODUCTS_REQUEST, FETCH_ALL_PRODUCTS_SUCCESS, FETCH_ALL_PRODUCTS_FAIL, CLEAR_ERRORS} from "../constants/products-constants";
+import { FETCH_NEW_PRODUCTS_SUCCESS, FETCH_NEW_PRODUCTS_FAIL } from './../constants/products-constants';
+import { FETCH_ALL_PRODUCTS_REQUEST, FETCH_ALL_PRODUCTS_SUCCESS, FETCH_ALL_PRODUCTS_FAIL, CLEAR_ERRORS, FETCH_NEW_PRODUCTS_REQUEST} from "../constants/products-constants";
 
 const productsInitialState = {
     products: []
@@ -21,6 +22,15 @@ export const productsReducer = (state = productsInitialState as any, action: any
         case FETCH_ALL_PRODUCTS_FAIL:
             return {loading: false, error: action.payload}
 
+        case FETCH_NEW_PRODUCTS_REQUEST:
+            return {}
+
+        case FETCH_NEW_PRODUCTS_SUCCESS:
+            return {}
+
+        case FETCH_NEW_PRODUCTS_FAIL:
+            return {}
+
         case CLEAR_ERRORS:
             return {error: null}
 
@@ -30,7 +40,7 @@ export const productsReducer = (state = productsInitialState as any, action: any
 }
 
 export const singleProductReducer = (state = singleProductInitialState as any, action) => {
-    
+
     switch(action.type) {
 
     }
