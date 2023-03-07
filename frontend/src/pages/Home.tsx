61de881fa46ad27372f05942dbf89a27d6b179f1
sabin2000pro <sabinlungu293@gmail.com>
2023-03-07 15:57:20 +0000

    Configuring the user routes to perform CRUD operations & edit start / end shift daates for regular staff users

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 
 const Home = () => {
+  const dispatch = useDispatch();
 
   return (
 
@@ -13,14 +15,14 @@ const Home = () => {
        <div className="flex flex-row gap-6 product-card-container">
          <div className="bg-white shadow-md rounded px-12 py-6 product-card">
 
-    <div className ="relative w-72 m-4 product-badge-container">
-        <div className = "absolute top-0 left-0 py-1 px-2 text-white product-badge font-bold rounded-tl">New</div>
-    </div>
+            <div className ="relative w-72 m-4 product-badge-container">
+                <div className = "absolute top-0 left-0 py-1 px-2 text-white product-badge font-bold rounded-tl">New</div>
+            </div>
     
-      <h2 className="text-lg font-medium mb-2 heading-secondary">Product 1</h2>
-      <p className ="text-gray-600 text-sm mb-2 product-description ">Product Description </p>
-      <button className="product-btn" type="button">Product Details</button>
-    </div>
+           <h2 className="text-lg font-medium mb-2 heading-secondary">Product 1</h2>
+           <p className ="text-gray-600 text-sm mb-2 product-description ">Product Description </p>
+          <button className="product-btn" type = "submit">Product Details</button>
+      </div>
   
 
     <div className="bg-white shadow-md rounded px-8 py-6 product-card">
