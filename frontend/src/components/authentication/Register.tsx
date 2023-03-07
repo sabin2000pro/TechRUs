9afd1e8e3acf42474bb7561f9e62f31a2154aedf
sabin2000pro <sabinlungu293@gmail.com>
2023-03-07 14:40:12 +0000

    Working on the register form functionality

--- frontend/src/components/authentication/Register.tsx
+++ frontend/src/components/authentication/Register.tsx
@@ -3,31 +3,45 @@ import MetaData from '../../layout/MetaData'
 import { Link } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
+import { register } from '../../actions/auth-actions'
+
 
 const Register = () => { // Register Component
   const navigate = useNavigate();
   const dispatch = useDispatch();
-  const {loading, error} = useSelector((state: any) => state.auth);
+
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const {loading, error, user, isAuthenticated} = useSelector((state: any) => state.auth);
 
   const onRegisterHandler = async (event) => {
      event.preventDefault();
+
+     dispatch(register(username, email, password) as any);
   }
 
   useEffect(() => {
-     const checkUserAuth = async () => {
 
+    if(error) {
+      console.log(`Register Error : `, error);
+    }
+
+     if(user) {
+       console.log(`Registered user : `, user);
      }
 
-     checkUserAuth();
-
-  }, [])
+  }, [user, error])
 
   return (
 
     <>
+
         <MetaData pageTitle = {`Register`} />
 
         {!loading && (
+
              <div className = "flex justify-center items-center h-screen login-container">
 
              <form method = "POST" onSubmit = {onRegisterHandler} className = "bg-white shadow-md rounded px-10 pt-8 pb-8 mb-4 login-form-container">
@@ -35,10 +49,9 @@ const Register = () => { // Register Component
                <h1 className = "heading-primary h-login">User Registration</h1>
              
                  <div className = "mb-4 login-container-inputs">
-             
-                 <label className ="block text-sm font-bold mb-2 login-username-label" htmlFor = "email">E-mail</label>
-             
-               <input className = "shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id  ="username" type="text" placeholder="Username" />
+            
+                     <label className ="block text-sm font-bold mb-2 login-username-label" htmlFor = "email">E-mail</label>
+                     <input value = {username} onChange = {(event) => setUsername(event.target.value)} className = "shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id = "username" type = "text" placeholder="Username" />
              
                </div>
              
