ff96d9194ca8c4af2bf4f56e1a4e00e5ab23e75a
sabin2000pro <sabinlungu293@gmail.com>
2023-03-08 09:42:00 +0000

    Updated the global store with the products / single product reducer

--- frontend/src/store.ts
+++ frontend/src/store.ts
@@ -1,12 +1,14 @@
-import { productsReducer } from './reducers/products-reducers';
+import { productsReducer, singleProductReducer } from './reducers/products-reducers';
 import {configureStore} from "@reduxjs/toolkit"
 import { authReducer, userReducer } from "./reducers/auth-reducer"
 
 export const store = configureStore({
+    
     reducer: {
         auth: authReducer,
         user: userReducer,
-        products: productsReducer
+        products: productsReducer,
+        singleProduct: singleProductReducer
     }
 
 })
