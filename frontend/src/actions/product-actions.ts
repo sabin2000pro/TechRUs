ccc24683000b63095707f4bbf1fa79d4a00e9cf9
sabin2000pro <sabinlungu293@gmail.com>
2023-03-08 10:01:32 +0000

    Working on the products actions / reducers

--- frontend/src/actions/product-actions.ts
+++ frontend/src/actions/product-actions.ts
@@ -1,15 +1,27 @@
 import axios from 'axios';
-import { FETCH_ALL_PRODUCTS_REQUEST, FETCH_ALL_PRODUCTS_SUCCESS, FETCH_ALL_PRODUCTS_FAIL } from '../constants/products-constants';
+import { FETCH_ALL_PRODUCTS_REQUEST, FETCH_ALL_PRODUCTS_SUCCESS, FETCH_ALL_PRODUCTS_FAIL, FETCH_NEW_PRODUCTS_REQUEST } from '../constants/products-constants';
+
+let PRODUCTS_ENDPOINT = `http://localhost:5404/api/v1/products`;
 
 export const fetchProducts = () => async (dispatch: any) => {
 
     try {
+        
       dispatch({type: FETCH_ALL_PRODUCTS_REQUEST});
 
-      const {data} = await axios.get(`http://localhost:5404/api/v1/products`)
+      const {data} = await axios.get(PRODUCTS_ENDPOINT);
+      console.log(`Fetched products : `, data);
+
+      dispatch({type: FETCH_ALL_PRODUCTS_SUCCESS, payload: data.products});
     } 
     
     catch(error) {
+
+      if(error) {
+        console.log(`Fetch products error: `, error);
+        dispatch({type: FETCH_ALL_PRODUCTS_FAIL, payload: error.data.response.message});
+      }
+
 
     }
 
@@ -17,9 +29,10 @@ export const fetchProducts = () => async (dispatch: any) => {
 }
 
 export const fetchNewProducts = () => async (dispatch: any) => {
-    
-    try {
 
+    try {
+        dispatch({type: FETCH_NEW_PRODUCTS_REQUEST});
+        const {data} = await axios.get(``)
     } 
     
     catch(error) {
