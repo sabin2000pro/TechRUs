944654def0ea72822bd03076ef0160f8059ff085
sabin2000pro <sabinlungu293@gmail.com>
2023-03-13 11:12:03 +0000

    Working on adding CRUD functionality for the shipping service + adding mock JSON data for shipping

--- backend/services/shipping-service/src/routes/shipping-routes.ts
+++ backend/services/shipping-service/src/routes/shipping-routes.ts
@@ -1,8 +1,9 @@
 import express from 'express';
 export const shippingRouter = express.Router();
+
 import { fetchShippingDetailsByID, fetchShippingDetails, createNewShipping, deleteShippingDetailsByID, editShippingDetails, editShippingStatus } from '../controllers/shipping-controller';
 
 shippingRouter.route('/').get(fetchShippingDetails).post(createNewShipping);
 shippingRouter.route('/:id').get(fetchShippingDetailsByID).delete(deleteShippingDetailsByID);
 
-shippingRouter.route('/:id/update-status').put()
+shippingRouter.route('/:id/update-status').put(editShippingStatus)
