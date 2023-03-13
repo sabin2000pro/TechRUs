a9a7136525ea9ddc2f371afeed9c303904d2aaa1
sabin2000pro <sabinlungu293@gmail.com>
2023-03-13 12:04:58 +0000

    Added error handler middleware to the orders and shipping services

--- backend/services/orders/src/middleware/error-handler.ts
+++ backend/services/orders/src/middleware/error-handler.ts
@@ -1,4 +1,32 @@
+require('dotenv').config();
+import { ErrorResponse } from '../utils/error-response';
+import {NextFunction} from 'express';
+import { StatusCodes } from 'http-status-codes';
 
-export const ordersErrorHandler = (err, request, response, next) => {
-    
+let ERROR_TYPES = ['CastError']
+
+export const errorHandler = (err, request: any, response: any, next: NextFunction) => {
+
+    let error = {...err};
+    error.message = err.message;
+
+    error.statusCode = err.statusCode;
+    error.stack = err.stack;
+
+    if(process.env.AUTH_SERVICE_NODE_ENV === 'development') {
+
+        if(err.code === 11000) {
+            const message = `Duplicate resource found on the server ${Object.keys(err.keyValue)}`;
+            error = new ErrorResponse(message, StatusCodes.BAD_REQUEST);
+        }
+
+        if(err.name === ERROR_TYPES[0]) {
+            const message = `Resource not found on the server. Invalid : ${err.path}`;
+            error = new ErrorResponse(message, StatusCodes.BAD_REQUEST);
+        }
+    }
+
+    console.log(error);
+
+    return response.status(error.statusCode).json({success: false, message: error.message, stack: error.stack});
 }
