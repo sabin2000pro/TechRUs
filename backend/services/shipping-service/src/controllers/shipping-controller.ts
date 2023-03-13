7380bfba3e4e07c1b54df5e732d07296edd0c879
sabin2000pro <sabinlungu293@gmail.com>
2023-03-13 11:02:30 +0000

    Writing the controller functions for the shipping service

--- backend/services/shipping-service/src/controllers/shipping-controller.ts
+++ backend/services/shipping-service/src/controllers/shipping-controller.ts
@@ -1,9 +1,25 @@
 import {Shipping} from '../model/shipping-model';
 
+export const fetchShippingDetails = async (request, response, next) => {
+    
+}
+
+export const fetchShippingDetailsByID = async (request, response, next) => {
+
+}
+
 export const createNewShipping = async (request, response, next) => {
 
 }
 
 export const editShippingDetails = async (request, response, next) => {
 
+}
+
+export const deleteShippingDetails = async (request, response, next) => {
+
+}
+
+export const deleteShippingDetailsByID = async (request, response, next) => {
+    
 }
