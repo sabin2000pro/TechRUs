a19153b00e5a0d374f406b7a1ef3507c608cc510
sabin2000pro <sabinlungu293@gmail.com>
2023-03-13 11:09:57 +0000

    Working on adding CRUD functionality for the shipping service + adding mock JSON data for shipping

--- backend/services/shipping-service/src/controllers/shipping-controller.ts
+++ backend/services/shipping-service/src/controllers/shipping-controller.ts
@@ -1,15 +1,31 @@
+import { StatusCodes } from 'http-status-codes';
+import {Response, NextFunction} from 'express';
 import {Shipping} from '../model/shipping-model';
+import asyncHandler from 'express-async-handler';
 
-export const fetchShippingDetails = async (request, response, next) => {
-    
-}
+export const fetchShippingDetails = asyncHandler(async (request, response, next): Promise<any> => {
+    const shipping = await Shipping.find();
 
-export const fetchShippingDetailsByID = async (request, response, next) => {
+    if(!shipping) {
 
-}
+    }
 
-export const createNewShipping = async (request, response, next) => {
+    return response.status(StatusCodes.OK).json({success: true, shipping});
+})
 
+export const fetchShippingDetailsByID = asyncHandler(async (request, response, next): Promise<any> => {
+    const id = request.params.id;
+    const shipping = await Shipping.findById(id);
+
+    if(!shipping) {
+
+    }
+
+    return response.status(StatusCodes.OK).json({success: true, shipping});
+})
+
+export const createNewShipping = async (request: any, response: Response, next: NextFunction): Promise<any> => {
+    const {} = request.body;
 }
 
 export const editShippingDetails = async (request, response, next) => {
@@ -22,4 +38,8 @@ export const deleteShippingDetails = async (request, response, next) => {
 
 export const deleteShippingDetailsByID = async (request, response, next) => {
     
+}
+
+export const editShippingStatus = async (reqeust, response, next) => {
+
 }
