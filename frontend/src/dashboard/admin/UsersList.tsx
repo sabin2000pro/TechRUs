b9128107f7c4b24ce096e489158532e12c0ea65e
sabin2000pro <sabinlungu293@gmail.com>
2023-03-14 12:56:39 +0000

    Working on add / remove items from basket functionality

--- frontend/src/dashboard/admin/UsersList.tsx
+++ frontend/src/dashboard/admin/UsersList.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
+import {useDispatch, useSelector} from 'react-redux';
+import { Link } from 'react-router-dom';
 
 const UsersList = () => {
+    const dispatch = useDispatch();
   return (
     <>
        <h2>Users List Here</h2>
