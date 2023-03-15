294cb6b784833b38607e941d626f0da5173bb4bd
sabin2000pro <sabinlungu293@gmail.com>
2023-03-15 11:44:16 +0000

    Writing code to create new product from admin dashboard

--- frontend/src/dashboard/admin/CreateProduct.tsx
+++ frontend/src/dashboard/admin/CreateProduct.tsx
@@ -3,15 +3,19 @@ import { useDispatch, useSelector } from 'react-redux';
 import { createNewProduct } from '../../actions/product-actions';
 
 const CreateProduct: React.FC = () => {
+  const dispatch = useDispatch();
   const [name, setName] = useState<string>("");
   const [description, setDescription] = useState<string>("");
   const [warranty, setWarranty] = useState<string>("");
   const [price, setPrice] = useState<number>(0);
   const [stockCount, setStockCount] = useState<number>(0);
   const [lowStockAlert, setLowStockAlert] = useState(0);
-  const dispatch = useDispatch();
+
   const {loading, error, user} = useSelector((state: any) => state.auth);
 
+  useEffect(() => {
+    console.log(`Logged In User : `, user);
+ }, [user])
 
   const handleProductCreate = async () => {
 
@@ -30,9 +34,6 @@ const CreateProduct: React.FC = () => {
 
   }
 
-  useEffect(() => {
-     console.log(`Logged In User : `, user);
-  }, [])
     
   return (
 
@@ -40,32 +41,44 @@ const CreateProduct: React.FC = () => {
 
 <div className = "flex justify-center items-center h-screen login-container">
 
-  <form onChange = {handleProductCreate} method = "POST" className = "bg-white shadow-md rounded px-10 pt-8 pb-8 mb-4 auth-container">
+  <form onChange = {handleProductCreate} method = "POST" className = "bg-white shadow-md rounded px-10 pt-8 pb-8 mb-4 auth-container cp-container">
 
-<h2 className = "heading-secondary mb-8">Create Product</h2>
+<h2 className = "heading-secondary mb-8">Inventory - Upload Product Data</h2>
 
   <div className = "mb-4 login-container-inputs username-container">
-        <label className ="block text-sm font-bold mb-2 login-username-label" htmlFor = "username">Name</label>
-        <input className = "shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id = "name" type = "text" placeholder = "Name" />
+        <label className ="block text-sm font-bold mb-2 login-username-label" htmlFor = "name">Name</label>
+        <input value = {name} onChange = {(event) => setName(event.target.value)} className = "shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id = "name" type = "text" placeholder = "Enter product name" />
   </div>
   
+  <div className = "mb-4 login-container-inputs email-container">
+        <label className ="block text-sm font-bold mb-2 login-username-label" htmlFor = "description">Description</label>
+        <input value = {description} onChange = {(event) => setDescription(event.target.value)} className = "shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id = "description" type = "text" placeholder="Enter product description" />
+  </div>
 
-    <div className = "mb-4 login-container-inputs email-container">
-        <label className ="block text-sm font-bold mb-2 login-username-label" htmlFor = "email">Description</label>
-        <input  className = "shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id = "description" type = "text" placeholder="Description" />
+  <div className = "mb-6 login-password-container">
+        <label className = "block text-sm font-bold mb-2 login-password-label" htmlFor = "password">Warranty</label>
+        <input value = {warranty} onChange = {(event) => setWarranty(event.target.value)} className ="shadow appearance-none border rounded w-full py-2 px-3 mb-3 leading-tight focus:outline-none focus:shadow-outline" id = "warranty" type = "text" placeholder = "Enter product warranty" />     
   </div>
 
 
   <div className = "mb-6 login-password-container">
-        <label className = "block text-sm font-bold mb-2 login-password-label" htmlFor = "password">Phone</label>
-   <input className ="shadow appearance-none border rounded w-full py-2 px-3 mb-3 leading-tight focus:outline-none focus:shadow-outline" id = "password" type = "password" placeholder = "Enter your password" />
-       
+        <label className = "block text-sm font-bold mb-2 login-password-label" htmlFor = "price">Price</label>
+        <input value = {price} onChange = {(event) => setPrice(event.target.value as any)} className ="shadow appearance-none border rounded w-full py-2 px-3 mb-3 leading-tight focus:outline-none focus:shadow-outline" id = "price" type = "number" placeholder = "Enter product price" />     
   </div>
 
+  <div className = "mb-6 login-password-container">
+        <label className = "block text-sm font-bold mb-2 login-password-label" htmlFor = "stock">Stock</label>
+        <input value = {stockCount} onChange = {(event) => setStockCount(event.target.value as any)} className ="shadow appearance-none border rounded w-full py-2 px-3 mb-3 leading-tight focus:outline-none focus:shadow-outline" id = "stock" type = "number" placeholder = "Enter product stock" />     
+  </div>
+
+  <div className = "mb-6 login-password-container">
+        <label className = "block text-sm font-bold mb-2 login-password-label" htmlFor = "password">Low Stock Alert</label>
+        <input value = {lowStockAlert} onChange = {(event) => setLowStockAlert(event.target.value as any)} className ="shadow appearance-none border rounded w-full py-2 px-3 mb-3 leading-tight focus:outline-none focus:shadow-outline" id = "price" type = "number" placeholder = "Enter low stock alert" />     
+  </div>
 
   <div className = "flex items-center justify-center login-btn-container">
 
-  <button className = "text-white font-bold py-2 px-4 rounded flex justify-center focus:outline-none focus:shadow-outline" type="submit">Create Product</button>
+  <button className = "text-white font-bold py-2 px-4 rounded flex justify-center focus:outline-none focus:shadow-outline cp-button" type="submit">Create Product</button>
 
   </div>
 
