7b5e827e45be2de2e5eb4e48da952568504a5424
sabin2000pro <sabinlungu293@gmail.com>
2023-03-15 15:16:19 +0000

    Working on the users reducer to allow store admins (managers) to view all users / edit start / shift dates

--- frontend/src/reducers/user-reducers.ts
+++ frontend/src/reducers/user-reducers.ts
@@ -1,4 +1,4 @@
-import { FETCH_USERS_REQUEST, FETCH_USERS_SUCCESS, FETCH_USERS_FAIL, FETCH_SINGLE_USER_REQUEST, FETCH_SINGLE_USER_SUCCESS } from './../constants/user-constants';
+import { FETCH_USERS_REQUEST, FETCH_USERS_SUCCESS, FETCH_USERS_FAIL, FETCH_SINGLE_USER_REQUEST, FETCH_SINGLE_USER_SUCCESS, FETCH_SINGLE_USER_FAIL, EDIT_USER_DETAILS_REQUEST, EDIT_USER_DETAILS_SUCCESS, EDIT_USER_SHIFTS_REQUEST, EDIT_USER_SHIFTS_SUCCESS, EDIT_USER_SHIFTS_FAIL } from './../constants/user-constants';
 
 const initialUserState = {
     users: []
@@ -34,6 +34,19 @@ export const singleUserReducer = (state = singleUserState as any, action: any) =
 
         case FETCH_SINGLE_USER_SUCCESS:
             return {...state, loading: false, user: action.payload}
+
+        case FETCH_SINGLE_USER_FAIL:
+            return {...state, loading: false, error: action.payload}
+
+        case EDIT_USER_SHIFTS_REQUEST:
+            return {loading: true}
+
+        case EDIT_USER_SHIFTS_SUCCESS:
+            const updatedUserShifts = {...state.user, startShiftDate: action.payload.startShiftDate, endShiftDate: action.payload.endShiftDate};
+            return {...state, loading: false, user: updatedUserShifts}
+
+        case EDIT_USER_SHIFTS_FAIL:
+            return {}
 
         default:
             return state
