1d658a7f2c0c69fcf2bf5b171cd38324f6ab3c41
sabin2000pro <sabinlungu293@gmail.com>
2023-03-15 14:20:23 +0000

    Removed use navigate dependency from the shipping details screen

--- frontend/src/components/shipping/ShippingDetails.tsx
+++ frontend/src/components/shipping/ShippingDetails.tsx
@@ -1,6 +1,5 @@
 import React, {useState, useEffect} from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { useNavigate } from 'react-router-dom';
 import { fetchLoggedInUser } from '../../actions/auth-actions';
 import { Link } from 'react-router-dom';
 
