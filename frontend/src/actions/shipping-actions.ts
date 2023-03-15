727185a50cbc433ee4b12fbc757e37e0b1fea709
sabin2000pro <sabinlungu293@gmail.com>
2023-03-15 14:13:11 +0000

    Working on the create new shipping action

--- frontend/src/actions/shipping-actions.ts
+++ frontend/src/actions/shipping-actions.ts
@@ -1,9 +1,11 @@
 import { SAVE_SHIPPING_INFO_REQUEST, SAVE_SHIPPING_INFO_SUCCESS, SAVE_SHIPPING_INFO_FAIL } from "../constants/shipping-constants";
 import axios from 'axios';
 
-export const createNewShipping = () => async (dispatch) => {
+export const createNewShipping = (address: string, city: string, country: string, postalCode: string) => async (dispatch) => {
     try {
         dispatch({type: SAVE_SHIPPING_INFO_REQUEST});
+
+        const {data} = await axios.post(`http://localhost:5411/api/v1/shipping`)
     } 
     
     catch(error) {
--- frontend/src/reducers/shipping-reducers.ts
+++ frontend/src/reducers/shipping-reducers.ts
@@ -3,8 +3,17 @@ import { SAVE_SHIPPING_INFO_REQUEST,  SAVE_SHIPPING_INFO_SUCCESS, SAVE_SHIPPING_
 const shippingInfoState = localStorage.getItem("shippingInfo") ? JSON.parse(localStorage.getItem("shippingInfo") as any) : {};
 
 export const shippingReducer = (state = shippingInfoState, action: any) => {
+    
     switch(action.type) {
 
+        case SAVE_SHIPPING_INFO_REQUEST:
+            return {loading: true}
+
+        case SAVE_SHIPPING_INFO_SUCCESS:
+            return {...state, loading: false, shipping: action.payload}
+
+        case SAVE_SHIPPING_INFO_FAIL:
+            return {loading: false, error: action.payload}
 
         default:
             return state;
