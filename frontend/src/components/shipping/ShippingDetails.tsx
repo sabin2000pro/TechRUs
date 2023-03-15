5890bc024357f3d8e29910c1244f5b94bcd9bf7a
sabin2000pro <sabinlungu293@gmail.com>
2023-03-15 14:19:54 +0000

    Working on the create new shipping action

--- frontend/src/components/shipping/ShippingDetails.tsx
+++ frontend/src/components/shipping/ShippingDetails.tsx
@@ -9,9 +9,14 @@ const ShippingDetails: React.FC = () => {
   const {shipping} = useSelector((state: any) => state.shipping);
   const {isAuthenticated, user} = useSelector((state: any) => state.auth)
 
-  useEffect(() => {
-      const loadUser = async () => {
+  const [address, setAddress] = useState<string>(shipping.address);
+  const [city, setCity] = useState<string>(shipping.city);
+  const [country, setCountry] = useState<string>(shipping.country);
+  const [postalCode, setPostalCode] = useState<string>(shipping.postalCode);
 
+  useEffect(() => {
+
+      const loadUser = async () => {
           dispatch(fetchLoggedInUser() as any);
       }
 
@@ -45,32 +50,30 @@ const ShippingDetails: React.FC = () => {
             <h2 className = "heading-secondary mb-8">Your Shipping Details</h2>
 
               <div className = "mb-4 login-container-inputs username-container">
-                    <label className ="block text-sm font-bold mb-2 login-username-label" htmlFor = "username">Address</label>
-                    <input className = "shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id = "address" type = "text" placeholder="Enter Shipping Address" />
+                  <label className ="block text-sm font-bold mb-2 login-username-label" htmlFor = "username">Address</label>
+                  <input className = "shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id = "address" type = "text" placeholder="Enter Shipping Address" />
               </div>
               
 
-    <div className = "mb-4 login-container-inputs email-container">
-        <label className ="block text-sm font-bold mb-2 login-username-label" htmlFor = "email">City</label>
-        <input className = "shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id = "username" type = "text" placeholder = "Username" />
-  </div>
+              <div className = "mb-4 login-container-inputs email-container">
+                  <label className ="block text-sm font-bold mb-2 login-username-label" htmlFor = "email">City</label>
+                  <input className = "shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id = "username" type = "text" placeholder = "Username" />
+            </div>
 
 
-    <div className = "mb-6 login-password-container">
-          <label className = "block text-sm font-bold mb-2 login-password-label" htmlFor = "password">Phone</label>
-          <input className ="shadow appearance-none border rounded w-full py-2 px-3 mb-3 leading-tight focus:outline-none focus:shadow-outline" id = "password" type = "password" placeholder = "Enter your password" />  
-    </div>
+              <div className = "mb-6 login-password-container">
+                    <label className = "block text-sm font-bold mb-2 login-password-label" htmlFor = "password">Phone</label>
+                    <input className ="shadow appearance-none border rounded w-full py-2 px-3 mb-3 leading-tight focus:outline-none focus:shadow-outline" id = "password" type = "password" placeholder = "Enter your password" />  
+              </div>
 
 
-  <div className = "flex items-center justify-center login-btn-container">
-      <button className = "text-white font-bold py-2 px-4 rounded flex justify-center focus:outline-none focus:shadow-outline" type="submit">Place Order</button>
-  </div>
+            <div className = "flex items-center justify-center login-btn-container">
+                <button className = "text-white font-bold py-2 px-4 rounded flex justify-center focus:outline-none focus:shadow-outline" type="submit">Place Order</button>
+            </div>
 
-
-</form>
-
-
+   </form>
 </div>
+
     </>
 
   )
