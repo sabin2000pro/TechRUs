2943b9d22c73b188aec30a2a2c901457a196d91f
sabin2000pro <sabinlungu293@gmail.com>
2023-03-16 13:33:00 +0000

    Working on the edit user start / end shifts

--- backend/services/api-gateway/src/controllers/api-gateway-controllers.ts
+++ backend/services/api-gateway/src/controllers/api-gateway-controllers.ts
@@ -3,15 +3,11 @@ import axios from 'axios';
 import {Response, NextFunction} from 'express';
 
 const LOGGED_IN_USER_ENDPOINT = `http://auth-service:5400/api/v1/auth/me`
+const COUPONS_ENDPOINT = `http://coupons-service:5477/api/v1/coupons`;
 const USER_ORDERS_ENDPOINT = `http://orders-service:5403/api/v1/orders/my-orders`
 
 export const fetchUserOrders = async (request: any, response: Response, next: NextFunction) => {
       const authorization = request.headers;
-
-      if(!authorization) {
-
-      }
-
       const ordersResponse = await axios.get(USER_ORDERS_ENDPOINT, {headers: {Authorization: authorization} })
       return response.status(StatusCodes.OK).json({success: true, ordersResponse});
 }
@@ -37,6 +33,7 @@ export const createProductWithAuthUser = async (request: any, response: Response
      if(error) {
         return console.error(error);
      }
+
     }
 
 
