9fc6ed3866ea1f846ccbed4bfc465bc74bf60a2a
sabin2000pro <sabinlungu293@gmail.com>
2023-03-16 13:38:51 +0000

    Testing the API gateway function to fetch user orders

--- backend/services/api-gateway/src/controllers/api-gateway-controllers.ts
+++ backend/services/api-gateway/src/controllers/api-gateway-controllers.ts
@@ -1,40 +1,17 @@
 import { StatusCodes } from 'http-status-codes';
 import axios from 'axios';
 import {Response, NextFunction} from 'express';
+import asyncHandler from 'express-async-handler';
 
 const LOGGED_IN_USER_ENDPOINT = `http://auth-service:5400/api/v1/auth/me`
 const COUPONS_ENDPOINT = `http://coupons-service:5477/api/v1/coupons`;
-const USER_ORDERS_ENDPOINT = `http://orders-service:5403/api/v1/orders/my-orders`
+const USER_ORDERS_ENDPOINT = `http://orders-service:5403/api/v1/orders`
 
-export const fetchUserOrders = async (request: any, response: Response, next: NextFunction) => {
+export const fetchUserOrders = asyncHandler(async (request: any, response: Response, next: NextFunction): Promise<any> => {
       const authorization = request.headers;
-      const ordersResponse = await axios.get(USER_ORDERS_ENDPOINT, {headers: {Authorization: authorization} })
-      return response.status(StatusCodes.OK).json({success: true, ordersResponse});
-}
+      const loggedInUserResponse = await axios.get(LOGGED_IN_USER_ENDPOINT, {headers: {Authorization: authorization} })
 
-export const createProductWithAuthUser = async (request: any, response: Response, next: NextFunction): Promise<any> => {
-
-    try {
-
-        const authResponse = await axios.get(LOGGED_IN_USER_ENDPOINT, {headers: {Authorization: request.headers.authorization} // Get logged in user
+      console.log(`Logged In user Response : `, loggedInUserResponse);
   
-      });    
-  
-      const userResponse = authResponse.data.user;
-      const user = userResponse._id;
-      const userEmail = userResponse.email;
-
-      request.user = {_id: user, email: userEmail};
-      return response.status(StatusCodes.OK).json({success: true, data: request.user});
-    }
-    
-    catch(error) {
-
-     if(error) {
-        return console.error(error);
-     }
-
-    }
-
-
-}
+      return response.status(StatusCodes.OK).json({success: true});
+})
--- backend/services/api-gateway/src/routes/api-gateway-routes.ts
+++ backend/services/api-gateway/src/routes/api-gateway-routes.ts
@@ -1,6 +1,5 @@
 import express from 'express';
 export const apiGatewayRouter = express.Router();
-import {fetchUserOrders, createProductWithAuthUser} from '../controllers/api-gateway-controllers';
+import {fetchUserOrders} from '../controllers/api-gateway-controllers';
 
-apiGatewayRouter.route('/customer-orders').get(fetchUserOrders);
-apiGatewayRouter.route('/get-user').get(createProductWithAuthUser)
+apiGatewayRouter.route('/my-orders').get(fetchUserOrders);
