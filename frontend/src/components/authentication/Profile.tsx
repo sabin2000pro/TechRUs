9246730704d6b2514bad79902cc486e8b4ceaaec
sabin2000pro <sabinlungu293@gmail.com>
2023-03-16 11:59:07 +0000

    Working on the CSS and styling HTML for the personal profile page

--- frontend/src/components/authentication/Profile.tsx
+++ frontend/src/components/authentication/Profile.tsx
@@ -5,7 +5,7 @@ import { fetchLoggedInUser } from '../../actions/auth-actions'
 
 const Profile = () => { // User personal profile page
     const dispatch = useDispatch();
-    const {isAuthenticated, user} = useSelector((state: any) => state.auth);
+    const {isAuthenticated, error, user} = useSelector((state: any) => state.auth);
 
     useEffect(() => {
        const loadUser = async () => {
@@ -29,11 +29,62 @@ const Profile = () => { // User personal profile page
     }, [dispatch])
 
   return (
+
     <>
+    
+      {isAuthenticated && !error && (
 
-      <h2 className = "heading-secondary">My Profile</h2>
+                  <div className = "flex flex-col justify-center items-center profile-container">
 
-    </>
+                    <div className = "w-1/3 p-4">
+
+                      <div className = "bg-blue rounded-lg shadow-lg p-6 h-30 mt-12">
+                            <h2 className="text-xl font-bold mb-4">Logged In As: {user.username}</h2>
+
+                        <div className="mb-4">
+
+                          <label className="block text-gray-700 font-bold mb-2" htmlFor="name">
+                            Username
+                          </label>
+
+                      <input className = "appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id = "username" type="text" value = {user.username} disabled />
+                        </div>
+
+                        <div className="mb-4">
+
+                           <label className="block text-gray-700 font-bold mb-2" htmlFor = "email">
+                              Email
+                             </label>
+
+                          <input className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type = "email" value = {user.email}
+                            disabled
+                          />
+
+                        </div>
+
+                        <div className="flex flex-row justify-center">
+
+                          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mr-4">
+                            Update Profile
+                          </button>
+
+                          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+                            Update Password
+                          </button>
+
+                        </div>
+                      </div>
+                    </div>
+
+
+                    <div className="w-2/3 p-4">
+                      {/* Main content goes here */}
+                    </div>
+
+                  </div>
+                )}
+              </>
+   
 
   )
 }
