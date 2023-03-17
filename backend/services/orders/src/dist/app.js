b5d329bfe4143e5ca2f8df8c0d13f4de002c81a1
sabin2000pro <sabinlungu293@gmail.com>
2023-03-17 15:13:54 +0000

    Working on creating new orders

--- backend/services/orders/src/dist/app.js
+++ backend/services/orders/src/dist/app.js
@@ -5,12 +5,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.app = void 0;
 require('dotenv').config();
+const orders_schema_1 = require("./database/orders-schema");
 const order_routes_1 = require("./routes/order-routes");
 const express_1 = __importDefault(require("express"));
 const morgan_1 = __importDefault(require("morgan"));
 const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
 exports.app = app;
+(0, orders_schema_1.connectOrdersSchema)();
 app.use(express_1.default.json());
 app.use((0, cors_1.default)({
     origin: '*',
