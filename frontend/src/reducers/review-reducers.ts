758b770cc3d203eb26cf46ebc5257fed7c12d49a
sabin2000pro <sabinlungu293@gmail.com>
2023-03-21 14:52:24 +0000

    Adding interface types to the reviews

--- frontend/src/reducers/review-reducers.ts
+++ frontend/src/reducers/review-reducers.ts
@@ -1,1 +1,21 @@
-import { CREATE_REVIEW_REQUEST, CREATE_REVIEW_SUCCESS, CREATE_REVIEW_FAIL} from "../constants/review-constants";
+import { CREATE_REVIEW_REQUEST, CREATE_REVIEW_SUCCESS, CREATE_REVIEW_FAIL, FETCH_REVIEWS_REQUEST, FETCH_REVIEWS_SUCCESS, FETCH_REVIEWS_FAIL} from "../constants/review-constants";
+
+interface Review {
+
+}
+
+interface ReviewsState {
+    reviews: Review[]
+}
+
+const reviewsState = {
+    reviews: []
+}
+
+const singleReviewState = {
+    review: {}
+}
+
+export const reviewsReducer = (state = reviewsState, action: any) => {
+
+}
