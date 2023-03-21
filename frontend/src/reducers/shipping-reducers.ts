9f47ba2e9faebfa59d34005fc4527726eb39780a
sabin2000pro <sabinlungu293@gmail.com>
2023-03-21 15:16:01 +0000

    Added the reviews and single review reducers to the global store.ts file

--- frontend/src/reducers/shipping-reducers.ts
+++ frontend/src/reducers/shipping-reducers.ts
@@ -1,10 +1,11 @@
-import { SAVE_SHIPPING_INFO_REQUEST, SAVE_SHIPPING_INFO_FAIL, SAVE_SHIPPING_INFO_SUCCESS, EDIT_SHIPPING_INFO_REQUEST, EDIT_SHIPPING_INFO_SUCCESS, EDIT_SHIPPING_STATUS_FAIL } from "../constants/shipping-constants";
+import { SAVE_SHIPPING_INFO_REQUEST, SAVE_SHIPPING_INFO_FAIL, SAVE_SHIPPING_INFO_SUCCESS, EDIT_SHIPPING_INFO_REQUEST, EDIT_SHIPPING_INFO_SUCCESS, EDIT_SHIPPING_STATUS_FAIL, EDIT_SHIPPING_INFO_FAIL } from "../constants/shipping-constants";
 
 const shippingInfoState = localStorage.getItem("shippingInfo") ? JSON.parse(localStorage.getItem("shippingInfo") as any) : {};
 
 interface IShippingState { // Default shipping state
     loading: boolean;
     error?: string;
+    message?: string;
 
     shipping: {
       address: string;
@@ -27,6 +28,15 @@ export const shippingReducer = (state: IShippingState = shippingInfoState, actio
 
         case SAVE_SHIPPING_INFO_FAIL:
             return {loading: false, error: action.payload, shipping: []}
+
+        case EDIT_SHIPPING_INFO_REQUEST:
+            return {...state, loading: false, error: undefined}
+
+        case EDIT_SHIPPING_INFO_SUCCESS:
+            return {...state, loading: false, error: undefined, message: action.payload}
+
+        case EDIT_SHIPPING_INFO_FAIL:
+            return {...state, loading: false, error: action.payload, message: undefined}
 
         default:
             return state;
