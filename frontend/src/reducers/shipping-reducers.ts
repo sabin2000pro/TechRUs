d0f06463912425644e6015302d5d1d3ed49d26ef
sabin2000pro <sabinlungu293@gmail.com>
2023-03-21 15:12:28 +0000

    Enforcing type safety guards to the shipping reducer / product reducers

--- frontend/src/reducers/shipping-reducers.ts
+++ frontend/src/reducers/shipping-reducers.ts
@@ -1,19 +1,32 @@
-import { SAVE_SHIPPING_INFO_REQUEST, SAVE_SHIPPING_INFO_FAIL, SAVE_SHIPPING_INFO_SUCCESS } from "../constants/shipping-constants";
+import { SAVE_SHIPPING_INFO_REQUEST, SAVE_SHIPPING_INFO_FAIL, SAVE_SHIPPING_INFO_SUCCESS, EDIT_SHIPPING_INFO_REQUEST, EDIT_SHIPPING_INFO_SUCCESS, EDIT_SHIPPING_STATUS_FAIL } from "../constants/shipping-constants";
 
 const shippingInfoState = localStorage.getItem("shippingInfo") ? JSON.parse(localStorage.getItem("shippingInfo") as any) : {};
 
-export const shippingReducer = (state = shippingInfoState, action: any) => {
+interface IShippingState { // Default shipping state
+    loading: boolean;
+    error?: string;
+
+    shipping: {
+      address: string;
+      city: string;
+      postalCode: string;
+      phoneNo: string;
+    }[]
+
+}
+
+export const shippingReducer = (state: IShippingState = shippingInfoState, action: any): IShippingState => {
     
     switch(action.type) {
 
         case SAVE_SHIPPING_INFO_REQUEST:
-            return {loading: true}
+            return {loading: true, error: undefined, shipping: []}
 
         case SAVE_SHIPPING_INFO_SUCCESS:
             return {...state, loading: false, shipping: action.payload}
 
         case SAVE_SHIPPING_INFO_FAIL:
-            return {loading: false, error: action.payload}
+            return {loading: false, error: action.payload, shipping: []}
 
         default:
             return state;
