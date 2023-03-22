076414fb63a3afe821cca20b6bd1421ea532403a
sabin2000pro <sabinlungu293@gmail.com>
2023-03-22 14:08:45 +0000

    Working on the order reducers and actions

--- frontend/src/reducers/order-reducers.ts
+++ frontend/src/reducers/order-reducers.ts
@@ -1,4 +1,4 @@
-import { UPDATE_ORDER_STATUS_REQUEST } from './../constants/orders-constants';
+import { UPDATE_ORDER_STATUS_REQUEST, UPDATE_ORDER_STATUS_SUCCESS, UPDATE_ORDER_STATUS_FAIL } from './../constants/orders-constants';
 import { FETCH_ORDERS_SUCCESS, FETCH_ORDERS_FAIL, FETCH_ORDERS_REQUEST, FETCH_SINGLE_ORDER_REQUEST, FETCH_SINGLE_ORDER_SUCCESS, FETCH_SINGLE_ORDER_FAIL, CREATE_ORDER_REQUEST, CREATE_ORDER_FAIL, CREATE_ORDER_SUCCESS } from "../constants/orders-constants";
 
 const initialOrdersState = {
@@ -29,13 +29,19 @@ export const orderReducer = (state = initialOrdersState as any, action: any): an
             return {loading: false, orders: [], error: action.payload}
 
         case CREATE_ORDER_REQUEST:
-            return {loading: true, error: undefined, orders: []}
+            return {loading: true, error: undefined, order: {}}
 
         case CREATE_ORDER_SUCCESS:
-            return {...state, loading: false, order: action.payload}
+            return {...state, loading: false, error: undefined, order: action.payload}
 
         case UPDATE_ORDER_STATUS_REQUEST:
-            return {loading: true, error: undefined, order: action.payload}
+            return {loading: true, error: undefined, order: {}}
+
+        case UPDATE_ORDER_STATUS_SUCCESS:
+            return {...state, loading: false, error: undefined, order: action.payload}
+
+        case UPDATE_ORDER_STATUS_FAIL:
+            return {loading: false, error: action.payload, order: {} }
 
         default:
             return state
