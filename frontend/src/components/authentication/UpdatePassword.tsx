38beada3995bcd085560bbffc7be65facc0dabaf
sabin2000pro <sabinlungu293@gmail.com>
2023-03-24 15:54:02 +0000

    ttest

--- frontend/src/components/authentication/UpdatePassword.tsx
+++ frontend/src/components/authentication/UpdatePassword.tsx
@@ -41,24 +41,24 @@ const UpdatePassword: React.FC = () => {
   return (
 
     <>
-
-     <MetaData pageTitle={`Update User Password`} />
-
       <div className = "flex justify-center items-center h-screen login-container">
 
         <form method = "POST" onSubmit = {onUpdatePasswordHandler} className = "bg-white shadow-md rounded px-10 pt-8 pb-8 mb-4 auth-container login-form">
 
           <h1 className = "heading-primary h-login">Update Password</h1>
 
-        <div className = "mb-4 login-container-inputs">
-            <label className ="block text-sm font-bold mb-2 login-username-label" htmlFor = "email">Current Password</label>
-            <input onChange = {(event) => setCurrentPassword(event.target.value)} className = "shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id = "currentpassword" type="text" placeholder="Current password" />
+            <div className = "mb-4 login-container-inputs">
+
+            <label className ="block text-sm font-bold mb-2 login-username-label" htmlFor = "email">E-mail</label>
+  
+          <input onChange = {(event) => setEmail(event.target.value)} className = "shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id  ="username" type="text" placeholder="Username" />
+
           </div>
 
           <div className = "mb-6 login-password-container">
-              <label className = "block text-sm font-bold mb-2 login-password-label" htmlFor = "password">New Password</label>
-              <input onChange = {(event) => setNewPassword(event.target.value)} className ="shadow appearance-none border rounded w-full py-2 px-3 mb-3 leading-tight focus:outline-none focus:shadow-outline" id = "password" type = "password" placeholder = "Enter your password" />
-              
+              <label className = "block text-sm font-bold mb-2 login-password-label" htmlFor = "password">Password</label>
+              <input onChange = {(event) => setPassword(event.target.value)} className ="shadow appearance-none border rounded w-full py-2 px-3 mb-3 leading-tight focus:outline-none focus:shadow-outline" id = "password" type = "password" placeholder = "Enter your password" />
+                {true && false ? <p className="text-red-500 text-xs italic">Please choose a password.</p> : ''}
           </div>
 
           <div className = "flex items-center justify-center login-btn-container">
