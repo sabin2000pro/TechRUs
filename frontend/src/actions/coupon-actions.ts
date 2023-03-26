d0e95112b6c4a5b7dd274dda455aa28cc4147a89
sabin2000pro <sabinlungu293@gmail.com>
2023-03-26 22:18:59 +0100

    Implementing code for e-mail verification and login MFA verification

--- frontend/src/actions/coupon-actions.ts
+++ frontend/src/actions/coupon-actions.ts
@@ -1,7 +1,6 @@
 import { FETCH_ALL_COUPONS_REQUEST, FETCH_ALL_COUPONS_SUCCESS, FETCH_SINGLE_COUPON_REQUEST, FETCH_SINGLE_COUPON_FAIL, FETCH_SINGLE_COUPON_SUCCESS} from "../constants/coupon-constants";
 import { Dispatch } from "redux"
 
-
 export const fetchAllCoupons = (keyword = '', page = 1) => async (dispatch: Dispatch): Promise<void> => {
 
     try {
--- frontend/src/components/authentication/VerifyEmail.tsx
+++ frontend/src/components/authentication/VerifyEmail.tsx
@@ -8,16 +8,44 @@ const VerifyEmail: React.FC = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  const {} = useSelector((state: any) => state.auth);
+  const [OTP, setOTP] = useState<string>("");
+
+  const {isAuthenticated, error, user} = useSelector((state: any) => state.auth);
   const [emailVerified, setEmailVerified] = useState(false);
+
+  const onEmailVerificationHandler = (event): void => {
+
+  }
 
   return (
 
     <>
 
     <MetaData pageTitle = {`E-mail Verification`} />
-    
-       <h2 className = "heading-secondary">Verify E-mail Address</h2>
+
+       <div className = "flex justify-center items-center h-screen login-container">
+
+              <form method = "POST" onSubmit = {onEmailVerificationHandler} className = "bg-white shadow-md rounded px-10 pt-8 pb-8 mb-4 auth-container login-form">
+
+                <h1 className = "heading-primary h-login">Verify Your E-mail Address</h1>
+
+                  <div className = "mb-4 login-container-inputs">
+
+                  <label className ="block text-sm font-bold mb-2 login-username-label" htmlFor = "email">E-mail</label>
+
+                <input onChange = {(event) => setOTP(event.target.value)} className = "shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id  ="username" type="text" placeholder="Username" />
+
+                </div>
+
+                <div className = "flex items-center justify-center login-btn-container">
+
+                <button className = "text-white font-bold py-2 px-4 rounded flex justify-center focus:outline-none focus:shadow-outline" type="submit">Login</button>
+
+                </div>
+
+            </form>
+
+         </div>
     </>
 
   )
