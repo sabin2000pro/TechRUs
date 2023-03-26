ff79d92fbd009eab33a0195007396da601b177ac
sabin2000pro <sabinlungu293@gmail.com>
2023-03-26 21:41:04 +0100

    Working on creating product reviews, editing and deleting reviews

---
+++ frontend/src/components/reviews/EditReview.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+
+const EditReview: React.FC = () => {
+
+  return (
+    <>
+
+      <h2>Edit Review Component</h2>
+
+    </>
+
+  )
+}
+
+export default EditReview
---
+++ frontend/src/components/reviews/Reviews.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Reviews = () => {
+
+  return (
+    <>
+      <h2>All Reviews Here</h2>
+    </>
+
+  )
+}
+
+export default Reviews
