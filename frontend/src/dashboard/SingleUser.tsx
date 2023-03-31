a5bff72954910238fc3c3cfc7e0c41477305b005
sabin2000pro <sabinlungu293@gmail.com>
2023-03-31 12:53:19 +0100

    Implemenged code to fetch the user ID from the URI - NOT RECOMMENDED IN PRODUCTION

--- frontend/src/dashboard/SingleUser.tsx
+++ frontend/src/dashboard/SingleUser.tsx
@@ -1,13 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { useParams } from 'react-router-dom';
+import { fetchUserByID } from '../actions/auth-actions';
 
 const SingleUser: React.FC = () => {
   const dispatch = useDispatch();
   const {} = useSelector((state: any) => state.singleUser);
   const {id} = useParams();
+  const [userLoaded, setUserLoaded] = useState<boolean>(false);
 
   console.log(`User ID to fetch : `, id);
+
+  useEffect(() => {
+      const loadSingleUser = async () => {
+
+      }
+
+      loadSingleUser();
+
+  }, [dispatch])
 
   return (
 
