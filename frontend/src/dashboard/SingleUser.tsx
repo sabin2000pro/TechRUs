4fce952a516dd5ebab2e9b53d1b50a32abf0d9aa
sabin2000pro <sabinlungu293@gmail.com>
2023-03-31 12:52:11 +0100

    test

--- frontend/src/dashboard/SingleUser.tsx
+++ frontend/src/dashboard/SingleUser.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import { useParams } from 'react-router-dom';
 
 const SingleUser: React.FC = () => {
   const dispatch = useDispatch();
-  
+  const {} = useSelector((state: any) => state.singleUser);
+  const {id} = useParams();
+
+  console.log(`User ID to fetch : `, id);
 
   return (
 
