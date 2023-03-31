aa04da56aebbae340bd8af820358f540cc427e5e
sabin2000pro <sabinlungu293@gmail.com>
2023-03-31 12:58:02 +0100

    Writing the code to fetch the details of a single user

--- frontend/src/dashboard/SingleUser.tsx
+++ frontend/src/dashboard/SingleUser.tsx
@@ -5,16 +5,21 @@ import { fetchUserByID } from '../actions/auth-actions';
 
 const SingleUser: React.FC = () => {
   const dispatch = useDispatch();
-  const {} = useSelector((state: any) => state.singleUser);
-  const {id} = useParams();
+  const {loading, user, error} = useSelector((state: any) => state.singleUser);
+  const {id} = useParams<string>();
   const [userLoaded, setUserLoaded] = useState<boolean>(false);
 
   console.log(`User ID to fetch : `, id);
 
   useEffect(() => {
+
       const loadSingleUser = async () => {
          try {
-            
+            dispatch(fetchUserByID(id as any) as any)
+
+            setUserLoaded((userLoaded) => !userLoaded);
+
+            console.log(`Loaded User Details : `, user);
          } 
          
          catch(error: any) {
