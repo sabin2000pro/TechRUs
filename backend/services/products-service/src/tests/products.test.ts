16b99eb84464f959bd8e01f1a84538def74e425c
sabin2000pro <sabinlungu293@gmail.com>
2023-04-06 12:01:02 +0100

    Changed DB connection URI in the products test from the authentication schema URI to products

---
+++ backend/services/products-service/src/tests/products.test.ts
@@ -0,0 +1,10 @@
+require('dotenv').config();
+import { StatusCodes } from 'http-status-codes';
+import request from "supertest"
+import mongoose from "mongoose"
+import {app} from '../app';
+ 
+// Establish connection to the authentication service database before running all the tests
+beforeAll(async () => {
+    return await mongoose.connect("mongodb+srv://sabin2000:123mini123@techrus-auth-schema.mfj4iaa.mongodb.net/?retryWrites=true&w=majority")
+})
