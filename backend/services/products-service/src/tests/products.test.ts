5ddc472f6bdbaaf339db46794d33d6c2d873a1f5
sabin2000pro <sabinlungu293@gmail.com>
2023-04-06 12:11:22 +0100

    Writing unit test sfor the create product API

--- backend/services/products-service/src/tests/products.test.ts
+++ backend/services/products-service/src/tests/products.test.ts
@@ -20,10 +20,15 @@ describe("Fetch All Products Test Suite", () => {
 describe("Fetch Product By ID Test Suite ", () => {
 
     it("Fetch Single Product By ID - Unit Test", async () => {
-        
+
         try {
             const productResponse = await request(app).get(`/api/v1/products/642a9d2201d0c6b3bc8155a2`);
-            console.log(`Product Response : `, productResponse);
+            const productIdValid = mongoose.Types.ObjectId.isValid("642a9d2201d0c6b3bc8155a2");
+            expect(productIdValid).toBe(true);
+
+            expect(productResponse.body.product).toHaveProperty('stockCount')
+            expect(productResponse.statusCode).toBe(StatusCodes.OK);
+            expect(productResponse.body.product.stockCount).toBeGreaterThan(0);
 
         } 
         
@@ -42,6 +47,22 @@ describe("Fetch Product By ID Test Suite ", () => {
 describe("Create New Product - Unit Test Suite", () => {
 
     it("Create New Product - Valid Data", async () => {
+        try {
 
+        } 
+        
+        catch(error) {
+
+        }
+    })
+
+    it("Create New Product - Invalid Data", async () => {
+        try {
+
+        }
+        
+        catch(error) {
+
+        }
     })
 })
