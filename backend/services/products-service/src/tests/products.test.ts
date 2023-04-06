d0ff523daf4a3f87c2eadd9a837317995d778d00
sabin2000pro <sabinlungu293@gmail.com>
2023-04-06 12:03:42 +0100

    Changed DB connection URI in the products test from the authentication schema URI to products

--- backend/services/products-service/src/tests/products.test.ts
+++ backend/services/products-service/src/tests/products.test.ts
@@ -20,12 +20,15 @@ describe("Fetch All Products Test Suite", () => {
 describe("Fetch Product By ID Test Suite ", () => {
 
     it("Fetch Single Product By ID - Unit Test", async () => {
+        
         try {
+            const productResponse = await request(app).get(`/api/v1/products/642a9d2201d0c6b3bc8155a2`);
+            console.log(`Product Response : `, productResponse);
 
         } 
         
         catch(error) {
-            
+
             if(error) {
                 throw new Error(error);
             }
